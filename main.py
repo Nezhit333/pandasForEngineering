e2bdfa902d30a8db52ef4f08f1ccd2dcb489140e
Nezhit333 <62287453+Nezhit333@users.noreply.github.com>
2020-10-10 18:52:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 import pandas as pd
-pd.set_option('max_colwidth', 600)
-pd.set_option('display.width', 600)
-df = pd.read_csv("06.05.2020.csv", sep=' ', encoding="cp1251")
-len(df.index)
-print(df.head(10))
+df = pd.read_csv('pokemon_data.csv')
+print(df.head(5))
